3f717561cd5f15010d04d4f0ddac996a1cce0724
Lucas Vaz <lucastarraf@hotmail.com>
2019-10-18 13:46:25 -0300

    genarateDataBase

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ import shutil
 
 
 def pega_top_jogos(fileOut):
-    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -X GET 'https://api.twitch.tv/helix/games/top' > "+fileOut)
+    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -s -X GET 'https://api.twitch.tv/helix/games/top' > "+fileOut)
 
 
 def pega_stream_jogos(nome, id):
 
-    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -X GET 'https://api.twitch.tv/helix/streams?first=20&game_id=" +
+    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -s -X GET 'https://api.twitch.tv/helix/streams?first=20&game_id=" +
               id+"' > streams/" + file_string(nome)+".json")
 
 
 
 def pega_b(nome, id):
     # https://api.twitch.tv/helix/users?id=44322889
-    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -X GET 'https://api.twitch.tv/helix/users?login=beethoven1997' > beethoven.json")
+    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -s -X GET 'https://api.twitch.tv/helix/users?login=beethoven1997' > beethoven.json")
 
 
 def pega_streamers(nome, id,game_name):
     # https://api.twitch.tv/helix/users?id=44322889
     filepath=id+"' > "+game_name+"/" + file_string(nome)+".json"
-    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -X GET 'https://api.twitch.tv/helix/users?id="+filepath )
+    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -s -X GET 'https://api.twitch.tv/helix/users?id="+filepath )
 
 
 def file_string(str):
@@ -31,9 +31,8 @@ def file_string(str):
 
 
 def pega_follow(nome,id):
-    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -X GET 'https://api.twitch.tv/helix/users/follows?from_id=" +
+    os.system("curl -H 'Client-ID: ioe7gqjfxhbxr0aqg4drweoul0jtrt' -s -X GET 'https://api.twitch.tv/helix/users/follows?limit=100&from_id=" +
               id+"' >" + file_string(nome)+".json")
-
 
 
 
@@ -60,7 +59,9 @@ def genarateDataBase():
         with open("streams/"+file_string(data['data'][i]['name']+".json")) as json_file:
             data_stream = json.load(json_file)
 
+        lista_streamers = []
         for j in range(len(data_stream['data'])):
+            print(j)
 
             path_user=path+"/"+file_string(data_stream['data'][j]['user_name'])
             if not os.path.exists(path_user):
@@ -71,19 +72,20 @@ def genarateDataBase():
 
             with open(path_user+"/"+file_string(data_stream['data'][j]['user_name']+".json")) as json_file:
                 data_streamer = json.load(json_file)
+                lista_streamers.append(data_streamer)
+
+
+        #print(data_stream['data'])
+        print(len(lista_streamers),len(data_stream['data']))
+        for j in range(len(data_stream['data'])):
+                # print(data_streamer['data'])
+                # print(k)
+                for k in range(len(lista_streamers)):
+                    followPath=path_user
+                    name0=data_stream['data'][j]['user_name']
+                    name=lista_streamers[k]["data"][0]["display_name"]
+                    print(name,name0)
+                #pega_follow(followPath+"/"+name+"Follows",data_streamer["data"][k]["id"])
 
                 
-            for k in range(len(data_streamer['data'])):
-                followPath=path_user
-                name=data_streamer["data"][k]["login"]
-                pega_follow(followPath+"/"+name+"Follows",data_streamer["data"][k]["id"])
-
-                
-
-
-
-
-
-
-#pega_stream_jogos("testeee","493057")
 genarateDataBase()
